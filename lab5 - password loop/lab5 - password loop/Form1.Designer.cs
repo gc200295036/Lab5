c0c008fb2bf7128eb6b17cfba363bea36d449e60
gc200295036 <nickbrazda95@hotmail.com>
2018-11-18 22:08:25 -0500

    Upload To GitHub
    Lab5 Programming Fundamentals - Password Message Loop created in Visual Studio using c#

---
+++ lab5 - password loop/lab5 - password loop/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace lab5___password_loop
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.emailInput = new System.Windows.Forms.TextBox();
+            this.passwordInput = new System.Windows.Forms.TextBox();
+            this.loginMessage = new System.Windows.Forms.Label();
+            this.loginMessage1 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(156, 118);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Login";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // emailInput
+            // 
+            this.emailInput.Location = new System.Drawing.Point(142, 66);
+            this.emailInput.Name = "emailInput";
+            this.emailInput.Size = new System.Drawing.Size(100, 20);
+            this.emailInput.TabIndex = 1;
+            // 
+            // passwordInput
+            // 
+            this.passwordInput.Location = new System.Drawing.Point(142, 92);
+            this.passwordInput.Name = "passwordInput";
+            this.passwordInput.Size = new System.Drawing.Size(100, 20);
+            this.passwordInput.TabIndex = 2;
+            // 
+            // loginMessage
+            // 
+            this.loginMessage.AutoSize = true;
+            this.loginMessage.Location = new System.Drawing.Point(366, 217);
+            this.loginMessage.Name = "loginMessage";
+            this.loginMessage.Size = new System.Drawing.Size(0, 13);
+            this.loginMessage.TabIndex = 3;
+            // 
+            // loginMessage1
+            // 
+            this.loginMessage1.AutoSize = true;
+            this.loginMessage1.ForeColor = System.Drawing.Color.Red;
+            this.loginMessage1.Location = new System.Drawing.Point(139, 154);
+            this.loginMessage1.Name = "loginMessage1";
+            this.loginMessage1.Size = new System.Drawing.Size(10, 13);
+            this.loginMessage1.TabIndex = 4;
+            this.loginMessage1.Text = "-";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(119, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(158, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Welcome! Please Sign In Below";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(132, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Username = \"nick brazda\"";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(234, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(138, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Password = \"password123\"";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(2, 200);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(128, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Made by : Nicolas Brazda";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 218);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.loginMessage1);
+            this.Controls.Add(this.loginMessage);
+            this.Controls.Add(this.passwordInput);
+            this.Controls.Add(this.emailInput);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox emailInput;
+        private System.Windows.Forms.TextBox passwordInput;
+        private System.Windows.Forms.Label loginMessage;
+        private System.Windows.Forms.Label loginMessage1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ lab5 - password loop/lab5 - password loop/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab5___password_loop
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+     // If/Else statement used so that my login message will change notifications depending on if the email + password are correct or incorrect//
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if ( emailInput.Text == "nick brazda" && passwordInput.Text == "password123" )
+            {
+              loginMessage1.Text = "Password accepted. Please continue";
+            }
+           else 
+            {
+                 loginMessage1.Text = "Incorrect password. Please try again.";
+            }
+            //CREATED BY: NICK BRAZDA - SIMPLE PASSWORD NOTIFICATION 
+            //Last Updated on Saturday November 17, 2018
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+        }
+    }
+}
